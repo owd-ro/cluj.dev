6b220abfa4fd4abf343645aee2041b201bc6b320
David Levente Dozsa <dozsalevi@gmail.com>
2024-02-04 10:39:01 +0000

    update all dependencies

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
